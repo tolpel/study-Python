606ac2f8a4a0d86f4de59481df16319c4a2e1f5f
tolpel <mizugame4649@gmail.com>
2018-11-19 01:31:52 +0900

    vsearch.py

---
+++ vsearch.py
@@ -0,0 +1,8 @@
+﻿def search4vowels(word):
+    """入力された単語内の母音を表示する。"""
+    vowels = set('aiueo')
+    found = vowels.intersection(set(word))
+    for vowel in found:
+        print(vowel)
+
+"""gitに家からあげてみた """
